8abd12a8ecb472c05732ff7422345fad84c67fb2
qXtnnkvD <groboviy228@gmail.com>
2021-06-30 16:51:18 +0300

    TechBase lesson 15

--- app/models/train.rb
+++ app/models/train.rb
@@ -1,7 +1,7 @@
 class Train < ApplicationRecord
   validates :number, presence: true
   has_many :tickets
-  has_many :carriages
+  has_many :carriages, ->(train) { order("number #{train.order_carriage ? 'ASC' : 'DESC'}") }
   belongs_to :route
   belongs_to :current_station, class_name: 'RailwayStation', foreign_key: :current_station_id
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_01_115101) do
+ActiveRecord::Schema.define(version: 2021_06_25_105540) do
 
   create_table "carriages", force: :cascade do |t|
+    t.integer "kind"
     t.integer "top_seats"
     t.integer "bottom_seats"
     t.integer "train_id"
