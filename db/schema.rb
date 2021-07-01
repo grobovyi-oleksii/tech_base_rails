1ef9231bfadcc46b9d4e551b9ecfe72a776f6a1d
qXtnnkvD <groboviy228@gmail.com>
2021-07-01 17:18:05 +0300

    TechBase lesson 16

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_25_105540) do
+ActiveRecord::Schema.define(version: 2021_06_30_131749) do
 
   create_table "carriages", force: :cascade do |t|
     t.integer "kind"
