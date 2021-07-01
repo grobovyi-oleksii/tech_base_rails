c1e4162bb8c1156ec57de38dc2e3416edca534c4
qXtnnkvD <groboviy228@gmail.com>
2021-07-01 17:18:05 +0300

    TechBase lesson 16

--- app/controllers/carriages_controller.rb
+++ app/controllers/carriages_controller.rb
@@ -1,29 +1,21 @@
 class CarriagesController < ApplicationController
-  before_action :set_carriage, only: %i[edit update destroy show]
+  before_action :set_train, only: %i[create new]
 
   def new
     @carriage = Carriage.new
   end
 
-  def show; end
-
-  def edit; end
-
-  def create
-    @carriage = Carriage.new(carriage_params)
-
-    if @carriage.save
-      redirect_to trains_url
-    else
-      render :new
-    end
+  def show
+    @carriage = Carriage.find(params[:id])
   end
 
-  def update
-    if @carriage.update(carriage_params)
-      redirect_to trains_url
+  def create
+    @carriage = @train.carriages.new(carriage_params)
+
+    if @carriage.save
+      redirect_to train_path(@train)
     else
-      redirect_to edit
+      render :new
     end
   end
 
@@ -34,11 +26,12 @@ class CarriagesController < ApplicationController
 
   private
 
-  def set_carriage
-    @carriage = Carriage.find(params[:id])
+  def set_train
+    @train = Train.find(params[:train_id])
   end
 
   def carriage_params
-    params.require(:carriage).permit(:type, :top_seats, :bottom_seats, :train_id, :side_bottom_seats, :side_top_seats, :eco_seats)
+    params.require(:carriage).permit(:type, :top_seats, :bottom_seats, :train_id, :side_bottom_seats, :side_top_seats,
+                                     :eco_seats)
   end
 end
--- app/models/route.rb
+++ app/models/route.rb
@@ -7,6 +7,19 @@ class Route < ActiveRecord::Base
   before_validation :set_name
 
   scope :ordered_railway_stations, -> { joins(:railway_stations_routes).order('railway_stations_routes.station_index') }
+  scope :with_station, ->(station) { Route.joins(:railway_stations).where('railway_stations.id = ?', station.id) }
+
+  def self.find_routes(start_station, end_station)
+    Route.with_station(start_station) & Route.with_station(end_station)
+  end
+
+  def first_station
+    railway_stations.first
+  end
+
+  def last_station
+    railway_stations.last
+  end
 
   private
 
@@ -17,4 +30,5 @@ class Route < ActiveRecord::Base
   def stations_count
     errors.add(:base, 'Route must have minimum 2 station') if railway_stations.count < 2
   end
+
 end
