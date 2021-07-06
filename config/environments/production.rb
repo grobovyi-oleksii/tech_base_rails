fdb3aeec83c18b0539862c19b237d90eb16c744f
qXtnnkvD <groboviy228@gmail.com>
2021-07-06 23:19:04 +0300

    setup mailer heroku #10

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,13 +5,13 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
+    user_name: 'apikey',
+    password: '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',
+    domain: 'railway-stations.herokuapp.com',
+    address: 'smtp.sendgrid.net',
     port: 587,
-    domain: 'gmail.com',
-    user_name: 'groboviyoleksiilab@gmail.com',
-    password: 'ijmyjnhgmsuirwhh',
-    authentication: 'plain',
-    # enable_starttls_auto: true
+    authentication: :plain,
+    enable_starttls_auto: true
   }
   # Settings specified here will take precedence over those in config/application.rb.
 
