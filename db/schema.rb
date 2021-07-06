9964b85282b447218dd5e6c8efa8f0ba74b60b82
qXtnnkvD <groboviy228@gmail.com>
2021-07-06 13:45:29 +0300

    Heroku commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_01_115101) do
+ActiveRecord::Schema.define(version: 2021_07_05_081640) do
 
   create_table "carriages", force: :cascade do |t|
     t.integer "kind"
