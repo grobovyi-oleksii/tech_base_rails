0d896c987892f54681dcb7bf22644ea2b0e55943
qXtnnkvD <groboviy228@gmail.com>
2021-07-07 11:26:54 +0300

    Fix lesson 15

--- app/models/railway_station.rb
+++ app/models/railway_station.rb
@@ -10,10 +10,6 @@ class RailwayStation < ApplicationRecord
       .order('railway_stations_routes.station_index').uniq
   }
 
-  def set_default_index
-    self.station_index ||= station_index(+1)
-  end
-
   def update_position(route, index)
     station_route = station_route(route)
     station_route&.update(station_index: index)
--- app/models/train.rb
+++ app/models/train.rb
@@ -5,7 +5,7 @@ class Train < ApplicationRecord
   belongs_to :route
   belongs_to :current_station, class_name: 'RailwayStation', foreign_key: :current_station_id
 
-  def count_seats_by_type(carriage_type, seats_type)
+  def counter_seats_by_type(carriage_type, seats_type)
     carriages.where(type: carriage_type).sum(seats_type)
   end
 
